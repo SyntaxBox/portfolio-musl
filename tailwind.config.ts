568e0d3226b65669dde33881ea2d872483774251
Abdelhamid Boudjit <qhamid@proton.me>
2024-06-15 14:26:46 +0100

    refactor: update config files

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./ui/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {},
+  plugins: [],
+};
+export default config;
